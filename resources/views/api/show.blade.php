295de4b69275b9ec6273b9b3f3bdd93273196233
xieruidong521 <957723538@qq.com>
2019-11-14 16:56:59 +0800

    api

--- resources/views/api/show.blade.php
+++ resources/views/api/show.blade.php
@@ -21,5 +21,25 @@
             htmlDecode:true,
         })
         document.getElementById('script').innerHTML=''
+        var os = function() {
+            var ua = navigator.userAgent,
+                isWindowsPhone = /(?:Windows Phone)/.test(ua),
+                isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+                isAndroid = /(?:Android)/.test(ua),
+                isFireFox = /(?:Firefox)/.test(ua),
+                isChrome = /(?:Chrome|CriOS)/.test(ua),
+                isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox && /(?:Tablet)/.test(ua)),
+                isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+                isPc = !isPhone && !isAndroid && !isSymbian;
+            return {
+                isTablet: isTablet,
+                isPhone: isPhone,
+                isAndroid : isAndroid,
+                isPc : isPc
+            };
+        }();
+        if(os.isPc){
+           document.getElementsByClassName('md-content')[0].style.width='800px';
+        }
     </script>
 @endsection
